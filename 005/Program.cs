fd02264812f9b7d1d974acc5d3a94ebf25cde76a
Kate <kath.tsv@yandex.ru>
2022-06-21 22:52:40 +0300

    Created 005

---
+++ 005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username -> ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "Marry")
+{
+    Console.WriteLine("Welcome back, Marry!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
